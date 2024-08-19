3949eefd5ebbf0b8c46bf1032a5c7c06d64a3c28
Tanishq Pandey <tanishqvatsa26@gmail.com>
2024-08-19 16:57:40 +0530

    create itinerary page ui added

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -1,23 +1,54 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import "./ContactFormStyles.css";
 
 function ContactForm() {
+
+    const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "568366da-d0e4-4ea2-8ac5-036d6c2eecef");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
 
     <div className='form-container'>
 
-        <h1>Send a message to us! <br></br>
-            (Aura -10000000)</h1>
+        <h1>Send a message to us!</h1>
 
-        <form>
+        <form onSubmit={onSubmit}>
 
-            <input placeholder='Name'></input>
-            <input placeholder='Email'></input>
-            <input placeholder='Subject'></input>
-            <textarea placeholder='Message' rows="4"></textarea>
-            <button>Send Message</button>
+            <input type="text" name='name' placeholder='Name' required></input>
+
+            <input type="email" name='email' placeholder='Email' required></input>
+
+            <input placeholder='Subject' required></input>
+
+            <textarea placeholder='Message' rows="4" name='message' required></textarea>
+
+            <button type="submit">Send Message</button>
 
         </form>
+
+        <span>{result}</span>
       
     </div>
 
--- frontend/src/components/CreateItineraryPage.js
+++ frontend/src/components/CreateItineraryPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import UserFunctionalityNavbar from './UserFunctionalityNavbar';
+import "./CreateItineraryPageStyles.css";
+import CreateImg from  "../assets/create.png";
 
 
 function CreateItineraryPage() {
@@ -7,6 +9,42 @@ function CreateItineraryPage() {
 
     <>    
     <UserFunctionalityNavbar/>
+
+    <div className='create-head'>
+    <h1>Create Your Itinerary</h1>
+    </div>
+
+    <div className="iti-page">
+    
+        <div className="create-iti-img">
+          <img src={CreateImg} alt="Create Itinerary" />
+        </div>
+
+        <div className="create-iti-form">
+          <form>
+
+            <input type="text" name="trip-title" placeholder="Trip Title" required />
+
+            <label for="dob"> From: </label>
+            <input type="date" id="from"/>
+
+            
+            <label for="dob"> To: </label>
+            <input type="date" id="to"/>
+
+            <textarea placeholder='Remarks' rows="10" name='message' required></textarea>
+
+            <button type="submit">Create Itinerary</button>
+
+          </form>
+        </div>
+      </div>
+
+
+      <h1>Your Itinerary</h1>
+      <div className='itineraries'>
+        <p>Itinerary here!</p>
+      </div>
     </>
     
   )
